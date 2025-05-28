0f59aca4be77ef8735cc5e5671ea38b5cb5f2d4f
ulyaphci <164328238+ulyaphci@users.noreply.github.com>
2025-05-28 09:15:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,7 @@ from datetime import datetime
 
 # NLTK setup for Streamlit Cloud
 nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
-if not os.path.exists(nltk_data_dir):
-    os.makedirs(nltk_data_dir)
+os.makedirs(nltk_data_dir, exist_ok=True)
 nltk.data.path.append(nltk_data_dir)
 nltk.download("punkt", download_dir=nltk_data_dir)
 nltk.download("stopwords", download_dir=nltk_data_dir)
